93bf8fe308a7cd904c06690b5d7e666391996e4f
Cybertox <oxetan@gmail.com>
2019-12-08 10:24:28 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-TOKEN = "1026378134:AAGdiGryUv2OfUOMpGBxxohxmO03wJjuZT8"
+import os
+#TOKEN = "1026378134:AAGdiGryUv2OfUOMpGBxxohxmO03wJjuZT8"
+
+URL = os.environ.get('URL')
+PORT = int(os.environ.get('PORT', '5000'))
+
+# telegram
+TOKEN = os.environ.get('TOKEN')
